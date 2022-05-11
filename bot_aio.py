0a3034bc48dd4d6c6980bd9af9de200d1112c50f
YunoGas <paninnnn@mail.ru>
2022-05-11 20:24:56 +0300

    v.0.1.0

--- bot_aio.py
+++ bot_aio.py
@@ -89,34 +89,33 @@ async def error_bot_blocked(update: types.Update, exception: NetworkError):
 ###############################
 
 # Хэндлер на команду /test1
-@dp.message_handler(commands="test1")
-async def cmd_test1(message: types.Message):
-    if message.chat.type == 'private':
-        await message.reply("Test 1 private")
-    elif message.chat.type == 'supergroup':
-        msg  = await message.reply("Это сообщение должно отредактироваться через 5 сек")
-        #msg = edit_msg(msg)
-        try:
-            asyncio.create_task(edit_msg(msg, "Это отредактированное сообщение удалиться через 5 сек", 5))
-            asyncio.create_task(delete_message(msg, 10))
-        except RuntimeError as ex:
-            if "There is no current event loop in thread" in str(ex):
-                loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(loop)
+# @dp.message_handler(commands="test1")
+# async def cmd_test1(message: types.Message):
+#     if message.chat.type == 'private':
+#         await message.reply("Test 1 private")
+#     elif message.chat.type == 'supergroup':
+#         msg  = await message.reply("Это сообщение должно отредактироваться через 5 сек")
+#         #msg = edit_msg(msg)
+#         try:
+#             asyncio.create_task(edit_msg(msg, "Это отредактированное сообщение удалиться через 5 сек", 5))
+#             asyncio.create_task(delete_message(msg, 10))
+#         except RuntimeError as ex:
+#             if "There is no current event loop in thread" in str(ex):
+#                 loop = asyncio.new_event_loop()
+#                 asyncio.set_event_loop(loop)
 
-                asyncio.create_task(edit_msg(msg, "Это отредактированное сообщение удалиться через 5 сек", 5))
-                asyncio.create_task(delete_message(msg, 10))
+#                 asyncio.create_task(edit_msg(msg, "Это отредактированное сообщение удалиться через 5 сек", 5))
+#                 asyncio.create_task(delete_message(msg, 10))
 
-        await bot.answer_callback_query()
-    else:
-        await message.reply("Test ")
+#         await bot.answer_callback_query()
+#     else:
+#         await message.reply("Test ")
 
 
 @dp.message_handler(commands="most_active_user")
 async def cmd_most_active(message: types.Message):
     if message.chat.type == 'private':
         if await is_admin(message.chat.id) == 1:
-            await message.reply("Привет админ")
             user = await get_most_active_user()
             user_id = user[1]
             name = user[2]
@@ -124,12 +123,12 @@ async def cmd_most_active(message: types.Message):
             is_admin_user = user[4]
             user_mess_count = user[5]
             reputation = user[6]
-            mess = f"Самый активный пользователь в чате:  {name} @{username} \n   \n Кол-во сообщений: {user_mess_count}"
+            mess = f"The most active user in chat:  {name} @{username} \n   \n Number of messages: {user_mess_count}"
             await message.answer(mess)
 
         else:
             print(message.chat.id)
-            await message.reply("Ты не админ")
+            await message.reply("You are not an admin")
 
     elif message.chat.type == 'supergroup':
         await message.reply("This command allow just in private")
@@ -155,7 +154,7 @@ async def cmd_admin2(message: types.Message):
         if await is_admin(message.chat.id) == 1:
             await message.reply("Привет админ")
             #socials = await get_all_soc()
-            await message.answer('Выберите пункты для администрирования:', reply_markup= ReplyKeyboards.admin_keyboard)
+            await message.answer('Select the administration points:', reply_markup= ReplyKeyboards.admin_keyboard)
 
         else:
             print(message.chat.id)
@@ -432,69 +431,69 @@ async def send_warning_chat():
     bot.send_message(CHAT_ID, (await get_current_text_by_name("warning"))[1])
 
 
-# in chat buttons callbacks
-@dp.callback_query_handler()
-async def navigate(callback_query: types.CallbackQuery):
-    user_id = callback_query.from_user.id
+# # in chat buttons callbacks
+# @dp.callback_query_handler()
+# async def navigate(callback_query: types.CallbackQuery):
+#     user_id = callback_query.from_user.id
 
-    if callback_query.data == 'Hello':
-        print(user_id)
-        return AnswerCallbackQuery(
-            callback_query.id,
-            text=f'Hi ! {user_id}',
-            show_alert=False
-        )
-    elif "edit_soc" in callback_query.data and await is_admin(user_id):
+#     if callback_query.data == 'Hello':
+#         print(user_id)
+#         return AnswerCallbackQuery(
+#             callback_query.id,
+#             text=f'Hi ! {user_id}',
+#             show_alert=False
+#         )
+#     elif "edit_soc" in callback_query.data and await is_admin(user_id):
         
-        name_soc = callback_query.data.split(" ")[1]
-        mess = name_soc+"\n\n"
-        info_soc = await get_soc_info_by_name(name_soc)
-        print(info_soc)
-        column_names = ['id', 'id_social', 'invite_text', 'url', 'img', 'time_edit']
-        empty = []
-        if info_soc is None:
-            Message = await bot.send_message(user_id,"Данные социальной сети не заполнены")
+#         name_soc = callback_query.data.split(" ")[1]
+#         mess = name_soc+"\n\n"
+#         info_soc = await get_soc_info_by_name(name_soc)
+#         print(info_soc)
+#         column_names = ['id', 'id_social', 'invite_text', 'url', 'img', 'time_edit']
+#         empty = []
+#         if info_soc is None:
+#             Message = await bot.send_message(user_id,"Данные социальной сети не заполнены")
 
-            mess = (f"{name_soc} add_atr url [ссылка] (добавить ссылку в соцсеть)" +
-                    f"{name_soc} add_atr invite_text [текст] (текст приглашения)"+
-                    f"{name_soc} add_atr img (после этой команды пришлите изображение)\n"
-                    )
-            Message = await bot.send_message(user_id,mess)
-        else:
-            for i in range(0, len(info_soc)):
-                #print(info_soc[i]) 
-                if info_soc[i] == None:
-                    empty.append(column_names[i]) 
-            if len(empty) >0:
-                mess+=f"В базе нет следующий полей <{name_soc}>:\n "
-                list_commands = []
-                for em in empty:
-                    if em == "url":
-                        list_commands.append(f"{name_soc} add_atr "+em+"\t[ссылка] (добавить ссылку в соцсеть)")
-                    elif em == "invite_text":
-                        list_commands.append(f"{name_soc} add_atr "+em+"\t[текст] (текст приглашения)")
-                    elif em == "img":
-                        list_commands.append(f"{name_soc} add_atr "+em+"\t (после этой команды пришлите изображение)\n")
-                    else:
-                        list_commands.append(f"{name_soc} add_atr "+em+f"\t (добавить атрибут {em} )")
-                    mess+= "  ["+em + "]  "
-                mess+="\n\n"
-                for z in list_commands:
-                    mess+=z+"\n"
-            Message = await bot.send_message(user_id,mess)
+#             mess = (f"{name_soc} add_atr url [ссылка] (добавить ссылку в соцсеть)" +
+#                     f"{name_soc} add_atr invite_text [текст] (текст приглашения)"+
+#                     f"{name_soc} add_atr img (после этой команды пришлите изображение)\n"
+#                     )
+#             Message = await bot.send_message(user_id,mess)
+#         else:
+#             for i in range(0, len(info_soc)):
+#                 #print(info_soc[i]) 
+#                 if info_soc[i] == None:
+#                     empty.append(column_names[i]) 
+#             if len(empty) >0:
+#                 mess+=f"В базе нет следующий полей <{name_soc}>:\n "
+#                 list_commands = []
+#                 for em in empty:
+#                     if em == "url":
+#                         list_commands.append(f"{name_soc} add_atr "+em+"\t[ссылка] (добавить ссылку в соцсеть)")
+#                     elif em == "invite_text":
+#                         list_commands.append(f"{name_soc} add_atr "+em+"\t[текст] (текст приглашения)")
+#                     elif em == "img":
+#                         list_commands.append(f"{name_soc} add_atr "+em+"\t (после этой команды пришлите изображение)\n")
+#                     else:
+#                         list_commands.append(f"{name_soc} add_atr "+em+f"\t (добавить атрибут {em} )")
+#                     mess+= "  ["+em + "]  "
+#                 mess+="\n\n"
+#                 for z in list_commands:
+#                     mess+=z+"\n"
+#             Message = await bot.send_message(user_id,mess)
         
         
 
-        return AnswerCallbackQuery(
-            callback_query.id,
-            text=f'{name_soc}',
-            show_alert=False
-        )
+#         return AnswerCallbackQuery(
+#             callback_query.id,
+#             text=f'{name_soc}',
+#             show_alert=False
+#         )
 
 
 
 
-    return AnswerCallbackQuery(callback_query.id)
+#     return AnswerCallbackQuery(callback_query.id)
 
 # handler for all simple msgs
 @dp.message_handler()
@@ -503,22 +502,23 @@ async def send_message(msg: types.Message):
     user_id = msg.from_user.id
 
     if chat_type == "private":
-        if "add_atr" in msg.text and await is_admin(user_id):
-            query = msg.text.split(" ")
-            #print(query)
-            if len(query)<4:
-                Message = await bot.send_message(user_id, "Не верный запрос")
-            name_soc = query[0]
-            if query[2] == "img":
-                pass
-            if query[2] == "url":
-                try:
-                    await edit_backup_soc_by_atr(name_soc=name_soc,atr="url", value=query[3])
-                except TypeError:
-                    await add_first_backup_soc_by_name_soc(name_soc=name_soc)
-                    await edit_backup_soc_by_atr(name_soc=name_soc,atr="url", value=query[3])
-                await bot.send_message(msg.chat.id, f'DONE, current state of {name_soc} :\n\t'+ str(await get_current_backup_by_time(name_soc)))
-                pass
+        pass
+        # if "add_atr" in msg.text and await is_admin(user_id):
+        #     query = msg.text.split(" ")
+        #     #print(query)
+        #     if len(query)<4:
+        #         Message = await bot.send_message(user_id, "Не верный запрос")
+        #     name_soc = query[0]
+        #     if query[2] == "img":
+        #         pass
+        #     if query[2] == "url":
+        #         try:
+        #             await edit_backup_soc_by_atr(name_soc=name_soc,atr="url", value=query[3])
+        #         except TypeError:
+        #             await add_first_backup_soc_by_name_soc(name_soc=name_soc)
+        #             await edit_backup_soc_by_atr(name_soc=name_soc,atr="url", value=query[3])
+        #         await bot.send_message(msg.chat.id, f'DONE, current state of {name_soc} :\n\t'+ str(await get_current_backup_by_time(name_soc)))
+        #         pass
 
     elif chat_type == "supergroup":
         chat_id =  msg["chat"]["id"]
