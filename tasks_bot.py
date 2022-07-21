65b572af2aec553cf2d198f7894b33be56471911
YunoGas <paninnnn@mail.ru>
2022-07-21 20:28:56 +0300

    Update tasks_bot.py

--- tasks_bot.py
+++ tasks_bot.py
@@ -452,7 +452,7 @@ async def create_session_client():
     await client.start()
 
 async def rewarding_users():
-    coin_name="Erg"
+    coin_name="kushti"
     top = await get_top_3_users()
     amount = await get_tip_amount_values()
 
@@ -827,7 +827,7 @@ async def scheduler():
     #Вознаграждение 3 топ пользователей. Обнуляет счётсчик сообщений в чате. Пока что каждое воскресенье в 20:00 по серверу - 21:00 по мск
     # Пока что тестим
     # aioschedule.every().sunday.at("20:00").do(rewarding_users)
-    aioschedule.every().thursday.at("19:25").do(rewarding_users)
+    aioschedule.every().thursday.at("19:30").do(rewarding_users)
     #aioschedule.every().minute.do(rewarding_users)
     #aioschedule.every().minute.do(get_tipper_balance)
 
