eb7a3707f2a03a5ca5f6ca65450fadce273e6ed8
YunoGas <paninnnn@mail.ru>
2022-07-21 20:21:16 +0300

    Update tasks_bot.py

--- tasks_bot.py
+++ tasks_bot.py
@@ -827,7 +827,7 @@ async def scheduler():
     #Вознаграждение 3 топ пользователей. Обнуляет счётсчик сообщений в чате. Пока что каждое воскресенье в 20:00 по серверу - 21:00 по мск
     # Пока что тестим
     # aioschedule.every().sunday.at("20:00").do(rewarding_users)
-    aioschedule.every().thursday.at("20:20").do(rewarding_users)
+    aioschedule.every().thursday.at("19:25").do(rewarding_users)
     #aioschedule.every().minute.do(rewarding_users)
     #aioschedule.every().minute.do(get_tipper_balance)
 
